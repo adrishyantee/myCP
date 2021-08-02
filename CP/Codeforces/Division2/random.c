d08a66bcf2e86230880d810510642446758aff17
adrishyantee <adrishyantee@gmail.com>
2021-08-02 20:12:36 +0530

    cf q

---
+++ CP/Codeforces/Division2/random.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main(){
+
+    int a,b;
+    char s[1000];
+    // scanf("%d %d", &a, &b);
+    scanf("%[^\n]%*c",s);
+    printf("%s",s);
+
+
+
+
+
+    return 0;
+}
